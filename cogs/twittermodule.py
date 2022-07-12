ec6f9fb9c178cb6b88140ce94c815a9282772bf3
Paerux <sinisterpaerux@hotmail.com>
2022-07-12 12:20:12 +0300

    log config
    log config encoding

--- cogs/twittermodule.py
+++ cogs/twittermodule.py
@@ -5,6 +5,8 @@ import tweepy
 from discord.ext import commands  # noqa
 from cogs.utilities import Utilities
 
+logger = logging.getLogger(__name__)
+
 
 class TwitterModule(commands.Cog):
     def __init__(self, bot):
@@ -12,7 +14,6 @@ class TwitterModule(commands.Cog):
 
     class TwitterStream(tweepy.StreamingClient):
         bot = None
-        logger = logging.getLogger(__name__)
 
         async def send_tweet(self, message):
             channel = self.bot.get_channel(891317992628031528)
@@ -22,33 +23,36 @@ class TwitterModule(commands.Cog):
             self.bot = bot
 
         def on_tweet(self, tweet):  # noqa
-            self.logger.info('on_tweet')
+            logger.info('on_tweet')
 
         def on_data(self, data):
-            tweet = json.loads(data)
-            self.logger.info(tweet)
-            if tweet['matching_rules'][0]['id'] == '1546402585137004544':
-                name = 'Botdener'
-            elif tweet['matching_rules'][0]['id'] == '1546402592909139969':
-                name = 'Çöp Ark'
-            else:
-                name = 'Unknown'
+            try:
+                tweet = json.loads(data)
+                logger.info(tweet)
+                if tweet['matching_rules'][0]['id'] == '1546402585137004544':
+                    name = 'Botdener'
+                elif tweet['matching_rules'][0]['id'] == '1546402592909139969':
+                    name = 'Çöp Ark'
+                else:
+                    name = 'Unknown'
 
-            self.bot.loop.create_task(self.send_tweet(name + " tweeted:"))
-            text = Utilities.case_sensitive_replace(tweet['data']['text'], 'lost ark', 'çöp ark')
-            self.bot.loop.create_task(self.send_tweet(text))
+                self.bot.loop.create_task(self.send_tweet(name + " tweeted:"))
+                text = Utilities.case_sensitive_replace(tweet['data']['text'], 'lost ark', 'çöp ark')
+                self.bot.loop.create_task(self.send_tweet(text))
+            except Exception as e:
+                logger.error(e)
 
         def on_errors(self, errors):
-            self.logger.error(errors)
+            logger.error(errors)
 
         def on_connect(self):
-            self.logger.info('on_connect')
+            logger.info('on_connect')
 
         def on_disconnect(self):
-            self.logger.info('on_disconnect')
+            logger.info('on_disconnect')
 
         def on_exception(self, exception):
-            self.logger.error(exception)
+            logger.error(exception)
 
         def on_request_error(self, code):
-            self.logger.error('on_request_error: ' + code)
+            logger.error('on_request_error: ' + code)
